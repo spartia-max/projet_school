bee38ffdcbf4da576a6b13ea3e272d17acf50668
Ertel nicolas <53335098+Ertelnicolas@users.noreply.github.com>
2019-09-12 08:32:28 +0200

    index.php
    cookie et session

--- public/index.php
+++ public/index.php
@@ -1,28 +1,35 @@
 <?php
 require '../app/Autoloader.php';
+    
 
-\app\autoloader::register();
-
-
-
-if(isset($_GET['p'])){
-    $p=$_GET['p'];
-
-}else{
-    $p='login';
+if(!empty($_POST['identification']))
+{
+    //protection du champ d'identification
+    $ident=htmlspecialchars($_POST['identification']);
+    //création du cookie
+    setcookie('utilisateur',$ident,time()+3600);
 }
 
-
+\App\autoloader::register();
 
 ob_start();
-if($p==='login'){
+//si on récupère des cookies ou s'il y a une session
+if(!empty($_COOKIE['utilisateur'])||(!empty($_SESSION['utilisateur'])))
+{
+    //on redirige
+    if(($_COOKIE['utilisateur']==='student')||($_SESSION['utilisateur']==='student')){
+        require '../views/student_vw.php';
+    }
+    if(($_COOKIE['utilisateur']==='teacher')||($_SESSION['utilisateur']==='teacher')){
+        require '../views/teacher_vw.php';
+    }
+    if(($_COOKIE['utilisateur']==='director')||($_SESSION['utilisateur']==='director')){
+        require '../views/director_vw.php';
+    }
+}else   //sinon on redirige vers login
+{
     require '../views/login_vw.php';
-}elseif($p==='article'){
-    require '../views/student_vw.php';
-}elseif($p==='categorie'){
-    require '../views/teacher_vw.php';
-}elseif($p==='director'){
-    require '../views/director_vw.php';
 }
+
 $content=ob_get_clean();
 require '../views/template/default.php';
